2840cc5a0f7bf0d422538d6f1d3a0cb8f5b4ba05
Valeria Lopez <vplopezm@gmail.com>
2023-02-08 17:28:24 -0300

    add new changes - copy option and patterns

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ inputColor.addEventListener("input", () => {
   document.querySelector(".circle:nth-child(5)").style.background = color;
   document.querySelector(".circle:nth-child(6)").style.background = color;
 });
+
+//Button to copy the color code
+let inputHexCode = document.getElementById("hex");
+let btnCopyAnswer = document.getElementById("copyAnswer");
+let spanCopyBlock = document.getElementById("copyBlock");
+spanCopyBlock.addEventListener("click", function (e) {
+  inputHexCode.select();
+  let successful = document.execCommand("copy");
+
+  if (successful) btnCopyAnswer.innerHTML = "Copied!";
+  else btnCopyAnswer.innerHTML = "Error";
+});
